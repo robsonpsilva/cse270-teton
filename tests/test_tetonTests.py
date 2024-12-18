d37608a8ba415b39362098eec3ab93918b5848b6
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-12-18 14:48:56 -0300

    smoke test headless

--- tests/test_tetonTests.py
+++ tests/test_tetonTests.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
+
 class TestTetonTests():
    
   # def setup_method(self, method):
